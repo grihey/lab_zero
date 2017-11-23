4ff146f49ea8f87cfb54c5a8f53497cc1a0632f5
Grigoriy Romanov <grigoriy.romanov@syntacore.com>
2017-11-23 12:44:41 +0300

    init commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int func(void);
+
+int        __attribute__((used)) global_int = 0;
+int static __attribute__((used)) global_int_static = 0;
+
+int __attribute__((used)) main(int argc, char ** argv[])
+{
+    for (int i = 0; i < 10; i++) {
+        global_int = 0xa * i;
+    }
+
+// func();
+    return 0;
+}
